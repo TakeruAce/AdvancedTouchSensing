85a71b0f6b089d7e669d76a77b1e5675ce2866c8
Masato Nomiyama <nomiyama@cyber.t.u-tokyo.ac.jp>
2018-10-31 16:28:55 +0900

    change a range of the pwm frequency and add digital pin 10 as output

--- Arduino/Sensing/src/Sensing.ino
+++ Arduino/Sensing/src/Sensing.ino
@@ -21,7 +21,9 @@
 #define CHK(x,y) (x & (1<<y))
 #define TOG(x,y) (x ^= (1<<y))
 
-#define N 160
+#define MIN_TOP 30
+#define MAX_TOP 255
+#define N MAX_TOP - MIN_TOP
 
 float results[N];
 float freq[N];
@@ -29,19 +31,21 @@ int sizeOfArray = N;
 
 void setup() {
   /*
-    Pin:            9 (16bit timer)
+    Pin:            9 & 10 (16bit timer)
     Wave:           High frequency PWM
     Method:         Low compare match
-    Clock division: None ()
+    Clock division: None
     Top:            110
     Threshold:      55
   */
-  TCCR1A = 0b10000010;
+  TCCR1A = 0b10100010;
   TCCR1B = 0b00011001;
-  ICR1 = 110;
-  OCR1A = 55;
+  ICR1 = MIN_TOP;
+  OCR1A = MIN_TOP / 2;
+  OCR1B = MIN_TOP / 2;
 
   pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
   pinMode(8,OUTPUT); // Sync test pin
 
   Serial.begin(115200);
@@ -58,13 +62,14 @@ void loop() {
     int v = analogRead(0); // Read response signal
     CLR(TCCR1B, 0);        // Stop generator
     TCNT1 = 0;             // Reload new frequency
-    ICR1 = d;
-    OCR1A = d / 2;
+    ICR1 = d + MIN_TOP;
+    OCR1A = (d + MIN_TOP) / 2;
+    OCR1B = (d + MIN_TOP) / 2;
     SET(TCCR1B, 0);        //-Restart generator
 
     results[d] = results[d] * 0.5 + (float)(v) * 0.5; //Filter results
 
-    freq[d] = d;
+    freq[d] = d + MIN_TOP;
   }
 
    PlottArray(1, freq, results);
